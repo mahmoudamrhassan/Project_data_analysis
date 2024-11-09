f79cbea55efd1120c3a7a33c8c68a13f66858e27
mahmoudamr5896 <mahmoudamr5896@gmail.com>
2024-11-09 21:26:45 +0200

    upload file and summery data

--- my-app/src/Components/Sidebar.js
+++ my-app/src/Components/Sidebar.js
@@ -2,9 +2,60 @@ import React from 'react';
 
 function Sidebar({ setCodeExample }) {
   const codeExamples = {
+
+// Hive Ql Journey
+readhive:`# Import required libraries
+from pyspark.sql import SparkSession
+# Create Spark session
+spark = SparkSession.builder.appName('DataProcessing').getOrCreate()
+# Read data using SQL
+df = spark.sql('''
+SELECT * 
+FROM dbname.table_name
+''')`,
+
+Fillnull:`
+# Process data static
+# Fill null values
+df = df.na.fill('Missing Value', ['column_name'])
+`,
+DropNull:`
+# Process data static
+# Drop rows with null values
+df = df.na.drop(subset=['column_name'])`,
+ChangeColumnType:`
+# Process data static
+from pyspark.sql.types import StringType, IntegerType
+from pyspark.sql.functions import col
+
+# Change column types
+df = df.withColumn('column_name', col('column_name').cast(StringType()))
+`,
+Addsequence:`
+# Process data 
+from pyspark.sql.window import Window
+from pyspark.sql.functions import row_number
+
+# Add sequence number
+window = Window.orderBy('id')
+df = df.withColumn('sequence', row_number().over(window))
+`,
+Renamecolumn:`
+# Process data static
+# Rename columns
+df = df.withColumnRenamed('column_name', 'new_column_name')
+
+`,
+Writeastable:`
+# Write processed data  SQL
+df.write.option('path', 'hdfs://PROD-HDFS-NN-HA/warehouse/tablespace/external/hive/ENter_DataBase_Name.db/Enter_newtable_name')/
+.saveAsTable('dbname.table_name')
+`,
+
     // Load Data
     readCSV: `from pyspark.sql import SparkSession\n\n# قراءة بيانات CSV\nspark = SparkSession.builder.appName("Read CSV").getOrCreate()\ndf = spark.read.csv("path/to/file.csv", header=True, inferSchema=True)\ndf.show()`,
     readSQL: `from pyspark.sql import SparkSession\n\n# قراءة بيانات من قاعدة بيانات SQL\nspark = SparkSession.builder.appName("Read SQL").getOrCreate()\ndf = spark.read.format("jdbc") \n.option("url", "jdbc:postgresql://hostname:port/dbname")\n.option("dbtable", "table_name")\n.option("user", "username")\n.option("password", "password")\n.load()\ndf.show()`,
+
     readOracle: `from pyspark.sql import SparkSession\n\n# قراءة بيانات من Oracle\nspark = SparkSession.builder.appName("Read Oracle").getOrCreate()\ndf = spark.read.format("jdbc") \n.option("url", "jdbc:oracle:thin:@//hostname:port/service_name")\n.option("dbtable", "table_name")\n.option("user", "username")\n.option("password", "password")\n.load()\ndf.show()`,
     readMongoDB: `from pyspark.sql import SparkSession\n\n# قراءة بيانات من MongoDB\nspark = SparkSession.builder.appName("Read MongoDB").getOrCreate()\ndf = spark.read.format("mongo")\n.option("uri", "mongodb://username:password@hostname:port/dbname.collection")\n.load()\ndf.show()`,
     writeCSV: `# كتابة البيانات إلى CSV\ndf.write.csv("path/to/output.csv")`,
@@ -20,7 +71,7 @@ function Sidebar({ setCodeExample }) {
     correlation: `#  حساب الارتباط بين الاعمده\n\n\ncorrelation = df.stat.corr("column1", "column2")\nprint("Correlation: ", correlation)`,
     // from pyspark.sql import SparkSessionspark = SparkSession.builder.appName("Null Values Example").getOrCreate()df = spark.read.csv("path/to/file.csv", header=True, inferSchema=True)
     nullValues: `\n\n# عدد القيم الفارغة في كل الاعمده \n\n\ndf.select([count(when(isnull(c), c)).alias(c) for c in df.columns]).show()`,
-    
+
     // Transformation Data
     dataPrep: `from pyspark.ml.feature import StandardScaler\nfrom pyspark.ml.linalg import Vectors\n\n# بيانات نموذجية للتقييس\nspark = SparkSession.builder.appName("MLlib Data Preparation").getOrCreate()\ndata = [(0, Vectors.dense([1.0, 0.1, -1.0]),), (1, Vectors.dense([2.0, 1.1, 1.0]),)]\ndf = spark.createDataFrame(data, ["id", "features"])\nscaler = StandardScaler(inputCol="features", outputCol="scaledFeatures")\nscalerModel = scaler.fit(df)\nscaledData = scalerModel.transform(df)\nscaledData.show()`,
     // New Examples
@@ -30,34 +81,99 @@ function Sidebar({ setCodeExample }) {
     // Visualization Data
     visualizationExample: `import matplotlib.pyplot as plt\nimport pandas as pd\n\n# مثال على التصور باستخدام Pandas وMatplotlib\n# يجب تحويل DataFrame Spark إلى DataFrame Pandas أولاً\npdf = df.toPandas()\nplt.figure(figsize=(10,6))\nplt.bar(pdf['column_name'], pdf['another_column'])\nplt.title('Visualization Example')\nplt.show()`
   };
-
   const explanations = {
-    readCSV: "هذا الكود يوضح كيفية قراءة البيانات من ملف CSV إلى DataFrame في Spark، مع تمكين استنتاج العنوان والهيكل.",
+    // رحلة Hive QL
+    readhive: "هذا الكود يوضح كيفية قراءة البيانات من جداول Hive باستخدام SQL في Spark.",
+    Fillnull: "هذا المثال يوضح كيفية ملء القيم الفارغة في عمود معين باستخدام قيمة افتراضية.",
+    DropNull: "هذا الكود يوضح كيفية حذف الصفوف التي تحتوي على قيم فارغة في عمود معين من DataFrame.",
+    ChangeColumnType: "يوضح هذا المثال كيفية تغيير نوع بيانات عمود في DataFrame باستخدام الدالة cast.",
+    Addsequence: "هذا الكود يوضح كيفية إضافة تسلسل أرقام إلى DataFrame باستخدام وظيفة نافذة.",
+    Renamecolumn: "يوضح هذا الكود كيفية إعادة تسمية عمود في DataFrame.",
+    Writeastable:'هذا الكود يوضح كيفية كتابة البيانات من جداول Hive باستخدام SQL في Spark.',
+    // تحميل البيانات
+    readCSV: "يوضح هذا الكود كيفية قراءة البيانات من ملف CSV إلى DataFrame في Spark مع استنتاج العنوان والهيكل تلقائيًا.",
     readSQL: "هذا المثال يوضح كيفية قراءة البيانات من قاعدة بيانات SQL باستخدام JDBC.",
-    readOracle: "هذا الكود يوضح كيفية قراءة البيانات من قاعدة بيانات Oracle باستخدام JDBC.",
-    readMongoDB: "هذا المثال يوضح كيفية قراءة البيانات من MongoDB.",
-    writeCSV: "هذا المقتطف يوضح كيفية كتابة البيانات من DataFrame إلى ملف CSV.",
-    
-    
-    // Explanation for printschemaa
-    printschemaa: "هذا المثال يوضح كيفية طباعة مخطط DataFrame الذي يعرض أسماء الأعمدة وأنواع بياناتها.",
-    basicStatistics: "هذا الكود يوضح كيفية حساب إحصائيات أساسية مثل المتوسط والحد الأقصى والحد الأدنى لعمود معين في DataFrame.",
-    groupBy: "هذا المثال يوضح كيفية تجميع البيانات بناءً على عمود معين وعدّ عدد التكرارات لكل قيمة.",
-    correlation: "هذا الكود يحسب معامل الارتباط بين عمودين في DataFrame.",
-    nullValues: "هذا المثال يظهر كيفية حساب عدد القيم الفارغة في كل عمود من أعمدة DataFrame.",
-    
-    dataPrep: "هذا الكود يوضح تقنيات إعداد البيانات في MLlib في Spark.",
-      // New Explanations
-  replaceDollar: "هذا الكود يوضح كيفية استبدال علامة الدولار ($) في عمود من DataFrame باستخدام الدالة regexp_replace.",
-  changeDataType: "هذا المثال يوضح كيفية تغيير نوع البيانات لعمود معين في DataFrame.",
-  changeDateFormat: "هذا الكود يوضح كيفية تغيير تنسيق التاريخ في عمود باستخدام الدالة date_format.",
-
-    visualizationExample: "هذا المثال يوضح كيفية استخدام مكتبة Matplotlib لتصور بيانات DataFrame باستخدام Pandas."
+    readOracle: "يوضح هذا الكود كيفية قراءة البيانات من قاعدة بيانات Oracle باستخدام JDBC.",
+    readMongoDB: "يوضح هذا المثال كيفية قراءة البيانات من MongoDB.",
+    writeCSV: "يوضح هذا المقتطف كيفية كتابة البيانات من DataFrame إلى ملف CSV.",
+  
+    // استكشاف تحليل البيانات
+    printschemaa: "يوضح هذا المثال كيفية طباعة مخطط DataFrame لعرض أسماء الأعمدة وأنواع بياناتها.",
+    basicStatistics: "يوضح هذا الكود كيفية حساب إحصائيات أساسية مثل المتوسط والحد الأقصى والحد الأدنى لعمود معين في DataFrame.",
+    groupBy: "يوضح هذا المثال كيفية تجميع البيانات وفقًا لعمود معين وعدّ عدد التكرارات لكل قيمة.",
+    correlation: "يوضح هذا الكود كيفية حساب معامل الارتباط بين عمودين في DataFrame.",
+    nullValues: "يوضح هذا المثال كيفية حساب عدد القيم الفارغة في كل عمود من أعمدة DataFrame.",
+  
+    // إعداد البيانات
+    dataPrep: "يوضح هذا الكود كيفية إعداد البيانات باستخدام مكتبة MLlib في Spark.",
+    replaceDollar: "يوضح هذا الكود كيفية استبدال علامة الدولار ($) في عمود من DataFrame باستخدام الدالة regexp_replace.",
+    changeDataType: "يوضح هذا المثال كيفية تغيير نوع البيانات لعمود معين في DataFrame.",
+    changeDateFormat: "يوضح هذا الكود كيفية تغيير تنسيق التاريخ في عمود باستخدام الدالة date_format.",
+  
+    // تصور البيانات
+    visualizationExample: "يوضح هذا المثال كيفية استخدام مكتبة Matplotlib لتصور بيانات DataFrame بعد تحويلها إلى DataFrame من Pandas."
   };
+  
+//   const explanations = {
+//     // Hive Ql Journey
+
+//     readhive:``,
+
+// Fillnull:`
+
+// `,
+// DropNull:`
+// `,
+// ChangeColumnType:`
+
+// `,
+// Addsequence:`
+
+// `,
+// Renamecolumn:`
+
+
+// `,
+//     readCSV: "هذا الكود يوضح كيفية قراءة البيانات من ملف CSV إلى DataFrame في Spark، مع تمكين استنتاج العنوان والهيكل.",
+//     readSQL: "هذا المثال يوضح كيفية قراءة البيانات من قاعدة بيانات SQL باستخدام JDBC.",
+//     readOracle: "هذا الكود يوضح كيفية قراءة البيانات من قاعدة بيانات Oracle باستخدام JDBC.",
+//     readMongoDB: "هذا المثال يوضح كيفية قراءة البيانات من MongoDB.",
+//     writeCSV: "هذا المقتطف يوضح كيفية كتابة البيانات من DataFrame إلى ملف CSV.",
+    
+    
+//     // Explanation for printschemaa
+//     printschemaa: "هذا المثال يوضح كيفية طباعة مخطط DataFrame الذي يعرض أسماء الأعمدة وأنواع بياناتها.",
+//     basicStatistics: "هذا الكود يوضح كيفية حساب إحصائيات أساسية مثل المتوسط والحد الأقصى والحد الأدنى لعمود معين في DataFrame.",
+//     groupBy: "هذا المثال يوضح كيفية تجميع البيانات بناءً على عمود معين وعدّ عدد التكرارات لكل قيمة.",
+//     correlation: "هذا الكود يحسب معامل الارتباط بين عمودين في DataFrame.",
+//     nullValues: "هذا المثال يظهر كيفية حساب عدد القيم الفارغة في كل عمود من أعمدة DataFrame.",
+//     Fillnull:`
+//     # Process data
+// # Fill null values
+// df = df.na.fill('Missing Value', ['column_name'])
+//     `,
+//     dataPrep: "هذا الكود يوضح تقنيات إعداد البيانات في MLlib في Spark.",
+//       // New Explanations
+//   replaceDollar: "هذا الكود يوضح كيفية استبدال علامة الدولار ($) في عمود من DataFrame باستخدام الدالة regexp_replace.",
+//   changeDataType: "هذا المثال يوضح كيفية تغيير نوع البيانات لعمود معين في DataFrame.",
+//   changeDateFormat: "هذا الكود يوضح كيفية تغيير تنسيق التاريخ في عمود باستخدام الدالة date_format.",
+
+//     visualizationExample: "هذا المثال يوضح كيفية استخدام مكتبة Matplotlib لتصور بيانات DataFrame باستخدام Pandas."
+//   };
 
   return (
-    <div style={{ width: '200px', backgroundColor: '#f1f1f1', padding: '20px' }}>
+    <div style={{ width: '250px', backgroundColor: '#f1f1f1', padding: '20px' }}>
       {/* <h3>مواضيع Spark</h3> */}
+      <h4>  [Hive Ql ]رحلة تحليل بيانات </h4>
+      {Object.keys(codeExamples).filter(key => key.endsWith("hive") || key ===  "Fillnull" || key ===  "DropNull"|| key ===  "ChangeColumnType"|| key ===  "Addsequence" || key ===  "Renamecolumn" ||  key === "Writeastable" ).map((key) => (
+        <button 
+          key={key}
+          onClick={() => setCodeExample({ code: codeExamples[key], explanation: explanations[key] })}
+          style={buttonStyle}
+        >
+          {key.replace(/([A-Z])/g, ' $1').trim()}
+        </button>
+      ))}
 
       {/* Load Data Section */}
       <h4>تحميل البيانات</h4>
@@ -73,7 +189,7 @@ function Sidebar({ setCodeExample }) {
 
       {/* Explore Data Analysis Section */}
       <h4>استكشاف تحليل البيانات</h4>
-      {Object.keys(codeExamples).filter(key => key.startsWith("basic") || key.startsWith("group") || key === "correlation" || key === "nullValues"|| key === "printschemaa").map((key) => (
+      {Object.keys(codeExamples).filter(key => key.startsWith("basic") || key.startsWith("group") || key === "correlation" || key === "nullValues" || key === "printschemaa" || key ===  "Fillnull" ).map((key) => (
         <button 
           key={key}
           onClick={() => setCodeExample({ code: codeExamples[key], explanation: explanations[key] })}
@@ -85,7 +201,7 @@ function Sidebar({ setCodeExample }) {
 
       {/* Transformation Data Section */}
       <h4>تحويل البيانات</h4>
-      {Object.keys(codeExamples).filter(key => key === "dataPrep" || key === "replaceDollar" || key === "changeDataType" || key === "changeDateFormat" || key === "dataPrep"  ).map((key) => (
+      {Object.keys(codeExamples).filter(key => key === "dataPrep" || key === "replaceDollar" || key === "changeDataType" || key === "changeDateFormat" || key === "dataPrep" ).map((key) => (
         <button 
           key={key}
           onClick={() => setCodeExample({ code: codeExamples[key], explanation: explanations[key] })}
